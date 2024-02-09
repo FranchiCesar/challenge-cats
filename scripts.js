baac51ef75ebfc169f3987993c4ba6253f98162e
Cesar <cesarfranchi111@gmail.com>
2024-02-09 00:16:17 -0300

    Started Project

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const $cat1Button = document.querySelector('.cat-1')
+const $cat2Button = document.querySelector('.cat-2')
+
+function changeCatImage1(){
+alert('teste')
+}
+function changeCatImage2(){
+alert('teste')
+}
+
+$cat1Button.addEventListener('click', changeCatImage1)
+$cat2Button.addEventListener('click', changeCatImage2)
